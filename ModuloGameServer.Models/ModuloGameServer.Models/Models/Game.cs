7ab8aa4dc3d0062a147fb12a9f7b0e8b6d347aa7
El Ege <ege2@mail.ru>
2021-05-07 09:02:33 +0300

    Change class Game

--- ModuloGameServer.Models/ModuloGameServer.Models/Models/Game.cs
+++ ModuloGameServer.Models/ModuloGameServer.Models/Models/Game.cs
@@ -1,77 +1,82 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 
 namespace ModuloGameServer.Models
 {
+    /// <summary>
+    /// Одна игра
+    /// </summary>
+    public class Game
+    {
         /// <summary>
-        /// Одна игра
+        /// Идентификатор
         /// </summary>
-        public class Game
-        {
-            /// <summary>
-            /// Идентификатор
-            /// </summary>
-            public int Id { set; get; }
+        public int Id { set; get; }
 
-            /// <summary>
-            /// Время начала игры
-            /// </summary>
-            public DateTime StartStamp { set; get; }
+        /// <summary>
+        /// Время начала игры
+        /// </summary>
+        public DateTime StartStamp { set; get; }
 
-            /// <summary>
-            /// Количество минут на один раунд
-            /// </summary>
-            public int MinutesPerRound { set; get; }
+        /// <summary>
+        /// Количество минут на один раунд
+        /// </summary>
+        public int MinutesPerRound { set; get; }
 
-            /// <summary>
-            /// Первый игрок
-            /// </summary>
-            public int? User1Id { set; get; }
+        /// <summary>
+        /// Первый игрок
+        /// </summary>
+        public int? User1Id { set; get; }
 
-            /// <summary>
-            /// Второй игрок
-            /// </summary>
-            public int? User2Id { set; get; }
+        /// <summary>
+        /// Второй игрок
+        /// </summary>
+        public int? User2Id { set; get; }
 
-            /// <summary>
-            /// Игра началась
-            /// </summary>
-            public bool IsStart { set; get; }
+        /// <summary>
+        /// Игра началась
+        /// </summary>
+        public bool IsStart { set; get; }
 
-            /// <summary>
-            /// Игра завершилась
-            /// </summary>
-            public bool IsFinish { set; get; }
+        /// <summary>
+        /// Игра завершилась
+        /// </summary>
+        public bool IsFinish { set; get; }
 
-            /// <summary>
-            /// Был таймаут
-            /// </summary>
-            public bool IsTimeout { set; get; }
+        /// <summary>
+        /// Был таймаут
+        /// </summary>
+        public bool IsTimeout { set; get; }
 
-            /// <summary>
-            /// Игра была отменена 
-            /// </summary>
-            public bool IsCancel { set; get; }
+        /// <summary>
+        /// Игра была отменена 
+        /// </summary>
+        public bool IsCancel { set; get; }
 
-            /// <summary>
-            /// Список ходов
-            /// </summary>
-            public virtual List<GameRound> Rounds { set; get; }
-
-            /// <summary>
-            /// Первый игрок
-            /// </summary>
-            public virtual User User1 { set; get; }
-
-            /// <summary>
-            /// Второй игрок
-            /// </summary>
-            public virtual User User2 { set; get; }
+        /// <summary>
+        /// Список ходов
+        /// </summary>
+        public virtual List<GameRound> Rounds { set; get; }
 
 
+        /// <summary>
+        /// Первый игрок
+        /// </summary>
+        [JsonIgnore]
+        public virtual User User1 { set; get; }
 
-            //public virtual GameResult Result { set; get; }        
+
+        /// <summary>
+        /// Второй игрок
+        /// </summary>
+        [JsonIgnore]
+        public virtual User User2 { set; get; }
 
 
-        }
+
+        //public virtual GameResult Result { set; get; }        
+
+
+    }
 }
