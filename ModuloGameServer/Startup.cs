077635957f10fe40716a4e2c32d3f7d950ea0a29
EvgeniiEzhov <evgenii.ezhov@stenn.com>
2021-09-23 11:31:24 +0300

    Add UseForwardedHeaders

--- ModuloGameServer/Startup.cs
+++ ModuloGameServer/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -40,13 +41,20 @@ namespace ModuloGameServer
                        .AllowAnyHeader();
             }));
 
-            services.AddTransient<IStartupFilter, ModuloGameDBStartupFilter>();
+            //services.AddTransient<IStartupFilter, ModuloGameDBStartupFilter>();
 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+
 
             if (true || env.IsDevelopment())
             {
@@ -58,6 +66,9 @@ namespace ModuloGameServer
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+
+
             //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
